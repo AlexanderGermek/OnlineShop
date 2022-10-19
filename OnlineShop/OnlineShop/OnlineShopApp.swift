61218d537c0bd03b03d0d9c458caae3654553758
Alexander Germek <alexander.hermek@gmail.com>
2022-10-19 19:48:36 +0600

    test commit

---
+++ OnlineShop/OnlineShop/OnlineShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineShopApp.swift
+//  OnlineShop
+//
+//  Created by Alexander Germek on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct OnlineShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
